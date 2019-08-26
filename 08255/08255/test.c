0821875f5473df5250a647e3a9a87ee27b410061
ZhouLan <1832311982@qq.com>
2019-08-26 19:27:06 +0800

    删除数组重复元素

--- 08255/08255/test.c
+++ 08255/08255/test.c
@@ -2,7 +2,55 @@
 
 #include<stdio.h>
 #include<stdlib.h> 
+#include<string.h>
 
+#if 1
+//删除排序数组中的重复项
+int removeDuplicates(int* nums, int numsSize){
+	int i = 0; int j = 0;
+	int cnt = 0;
+	while (numsSize--){
+		if (nums[i] == nums[i + 1]){
+			cnt++;
+		}
+		else {
+			nums[j++] = nums[i];
+		}
+		i++;
+	}
+	for (int i = 0; i < cnt; i++){
+		printf("%d ", nums[i]);
+	}
+	printf("\n");
+	return cnt;
+}
+
+int removeDuplicates2(int* nums, int numsSize){
+	int cnt = 1;
+	int i ;
+	for (i = 0; i < numsSize-1; i++){
+		if (nums[i] != nums[i + 1]){
+			nums[cnt] = nums[i+1];
+			cnt++;
+		}
+	}
+	for (int i = 0; i < cnt; i++){
+		printf("%d ", nums[i]);
+	}
+	printf("\n");
+		return cnt;
+}
+
+
+int main(){
+	int arr[] = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4, };
+	printf("%d\n", removeDuplicates2(arr, 10));
+	system("pause");
+	return 0;
+}
+#endif
+
+#if 0
 //两串字符串的最大长度
 char * longTwoCommon(char *str1, char *str2){
 	char *p = str1;
@@ -17,6 +65,8 @@ char * longTwoCommon(char *str1, char *str2){
 	*p = '\0';
 	return str1;
 }
+
+//多组字符串的最大前缀
 char * longestCommonPrefix(char ** strs, int strsSize){
 	if (strsSize == 0) return "";
 	int len = strlen(strs[0]);
@@ -46,3 +96,4 @@ int main(){
 	system("pause");
 	return 0;
 }
+#endif 
