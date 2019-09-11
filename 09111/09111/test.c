cf3d8630ab0c5b7cf1e82f67960a23b29611e3e9
ZhouLan <1832311982@qq.com>
2019-09-11 21:36:01 +0800

    大数阶乘

--- 09111/09111/test.c
+++ 09111/09111/test.c
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#if 0
 //爬楼梯 其实就是斐波那契数列
 int climbStairs1(int i,int n){
 	if (i > n) return 0;
@@ -35,3 +36,42 @@ int main(){
 	system("pause");
 	return 0;
 }
+#endif 
+
+#if 1
+//n!
+#define N 10000
+int main()
+{
+	int r[N] = { 1 };
+	int i, j;
+	int k = 0, l = 0;
+	for (i = 1; i <= 50; i++)
+	{
+		for (j = 0; j <= l; j++)
+		{
+			r[j] = r[j] * i + k;
+			/* 以万为进制，提高运算速度 */
+			k = r[j] / 10000;
+			r[j] = r[j] % 10000;
+		}
+		if (k)
+		{
+			l++;
+			r[j] = k;
+			k = 0;
+		}
+		j = l;
+		/* 先输出第一个数，防止首位出现0 */
+		printf("%d!=%d", i, r[j--]);
+		for (; j >= 0; j--)
+		{
+			/* 输出其余的数，因为万进制，需要补0 */
+			printf("%04d", r[j]);
+		}
+		printf("\n");
+	}
+	system("pause");
+	return 0;
+}
+#endif
