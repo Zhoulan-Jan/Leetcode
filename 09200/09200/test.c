688fd6f53137fa1e4f02e5cc11e6e66ae73f0754
ZhouLan <1832311982@qq.com>
2019-09-20 23:41:45 +0800

    1的个数

--- 09200/09200/test.c
+++ 09200/09200/test.c
@@ -1,1 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int hammingWeight(uint32_t n) {
+	int cnt = 0;
+	while (n){
+		n = n & (n - 1);
+		cnt++;
+	}
+	return cnt;
+}
