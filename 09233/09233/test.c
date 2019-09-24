a8acfafad956a9c6c24ba6df776d4288ec0f0af4
ZhouLan <1832311982@qq.com>
2019-09-24 08:54:17 +0800

    简单版八皇后

--- 09233/09233/test.c
+++ 09233/09233/test.c
@@ -3,21 +3,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-//°Ë»Êºó
-#define NUM 4
+//¼òµ¥°æ°Ë»Êºó
+#define NUM 8
 int Q[10] = { 0 };
 void queen(int cur){
 	if (cur == NUM) {
 		for (int i = 0; i < NUM; i++){
 			printf("%d ", Q[i]);
 		}
+		printf("\n");
 	}
 	else {
 		for (int i = 0; i < NUM; i++){
 			Q[cur] = i;
+			int ok = 1;
 			for (int j = 0; j < cur; j++){
-
+				if (Q[cur] == Q[j] || cur - Q[cur] == j - Q[j] || cur + Q[cur] == j + Q[j]){
+					ok = 0;
+					break;
+				}
 			}
+			if (ok) queen(cur + 1);
 		}
 	}
 }
