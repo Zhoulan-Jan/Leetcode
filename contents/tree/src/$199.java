ee8cf582269cfb5534c57df48788287c50f27c7b
ZhouLan <1832311982@qq.com>
2020-03-22 20:21:35 +0800

    二叉树的右视图

---
+++ contents/tree/src/$199.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+
+public class $199 {
+    //深度优先搜索
+    public static List<Integer> rightSideView(TreeNode root) {
+        Map<Integer, Integer> map = new HashMap<>(); //深度 -- 节点值
+        int maxDepth = -1;
+        Stack<TreeNode> nodeStack = new Stack<>(); //存放结点的栈
+        Stack<Integer> depthStack = new Stack<>(); //存放深度的栈
+        nodeStack.push(root);
+        depthStack.push(0);
+
+        while (!nodeStack.empty()) {
+            TreeNode node = nodeStack.pop();
+            int depth = depthStack.pop();
+
+            if (node != null) {
+                maxDepth = Math.max(maxDepth, depth);
+                if (!map.containsKey(depth)) {
+                    map.put(depth, node.val);
+                }
+                nodeStack.push(node.left);
+                nodeStack.push(node.right);
+                depthStack.push(depth+1);
+                depthStack.push(depth+1);
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int depth = 0; depth <= maxDepth; depth++) {
+            res.add(map.get(depth));
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode two = new TreeNode(2);
+        TreeNode three = new TreeNode(3);
+        TreeNode four = new TreeNode(4);
+        TreeNode seven = new TreeNode(7);
+
+        root.left = two;
+        root.right = three;
+        two.left = four;
+        four.right = seven;
+
+        List<Integer> t = rightSideView(root);
+        for (int i = 0; i < t.size(); i++) {
+            System.out.print(t.get(i) + " ");
+        }
+    }
+}
