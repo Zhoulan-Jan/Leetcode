6da06dbf211c6d123150e472eb9698d1a5cf88b4
ZhouLan <1832311982@qq.com>
2022-07-21 12:10:29 +0800

    N皇后

---
+++ c_contents/12283/12283/test.cpp
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//51.N皇后
+
+#include<cstdio>
+#include<cstdlib>
+#include<vector>
+#include<string>
+#include<iostream>
+#include<ctime>
+using namespace std;
+
+class Solution {
+public:
+	//判断位置是否合法
+	bool isValid(vector<pair<int, int>>& solution, int row, int col) {
+		//不在同一列，同一斜线
+		for (auto& pos : solution) {
+			//first表示横坐标，second表示纵坐标
+			if (pos.second == col
+				|| pos.first + pos.second == row + col
+				|| pos.first - pos.second == row - col) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	//pair<int, int> C++内部的结构体 
+	//solutions放的是坐标集合， solution放的是一次成功皇后的坐标
+	void DFS(vector<vector<pair<int, int>>>& solutions,
+		vector<pair<int, int>>& solution, int curRow, int n) {
+		//所有行的皇后位置已确定，存下这种解决方案 ，然后向前回退
+		if (curRow == n) {
+			solutions.push_back(solution);
+			return;
+		}
+		//每一行尝试每一列的位置
+		for (int i = 0; i < n; i++) {
+			//如果位置不冲突，将当前位置保存
+			if (isValid(solution, curRow, i)) {
+				solution.push_back(make_pair(curRow, i));
+				DFS(solutions, solution, curRow + 1, n);
+				solution.pop_back();
+			}
+		}
+	}
+	vector<vector<string>> solveNQueens(int n) {
+		vector<vector<string>> ret;
+		vector<vector<pair<int, int>>> solutions;
+		vector<pair<int, int>> solution;
+		DFS(solutions, solution, 0, n);
+		for (auto& e : solutions) {
+			vector<string> retStr(n, string(n, '#'));
+			for (auto& pos : e) {
+				retStr[pos.first][pos.second] = 'Q';
+			}
+			ret.push_back(retStr);
+		}
+		return ret;
+	}
+};
+
+int main() {
+	clock_t run_start = clock();
+	Solution s;
+	vector<vector<string>> res = s.solveNQueens(8);
+	for (int i = 0; i < res.size(); i++) {
+		for (int j = 0; j < res[i].size(); j++) {
+			cout << res[i][j] << endl;
+		}
+		cout << endl;
+	}
+	clock_t run_end = clock();
+	double runtime = ((double)run_end - (double)run_start) / CLOCKS_PER_SEC;
+	printf("Run Time: %.3f s \n", runtime);
+	system("pause");
+	return 0;
+}
