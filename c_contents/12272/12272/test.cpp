b42bbb88eec52e6c51b9b07263fa44854e86f653
ZhouLan <1832311982@qq.com>
2022-07-21 12:08:10 +0800

    迷宫问题

---
+++ c_contents/12272/12272/test.cpp
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<cstdio>
+#include<cstdlib>
+#include<vector>
+#include<iostream>
+#include<queue>
+using namespace std;
+
+//BFS
+//迷宫问题
+
+class position
+{
+public:
+	int x;
+	int y;
+	position(int xx, int yy) {
+			x = xx;
+			y = yy;
+		}
+	~position();
+
+private:
+
+};
+
+//position::position(int xx, int yy)
+//{
+//}
+
+position::~position()
+{
+}
+
+bool BFS(vector<vector<int>> &grid, int startx, int starty, int endx, int endy) {
+	static int pos[4][2] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+	int row = grid.size();
+	int col = grid[0].size();
+	//标记 保证不搜索重复的点，避免死循环
+	vector<vector<int>> visit(row, vector<int>(col, 0));
+	//起始位置入队
+	queue<position> q;
+	q.push(position(startx,starty));
+	visit[startx][starty] = 1; //表示已搜索该点
+	//搜索
+	while (!q.empty()) {
+		//取队首元素
+		position curPos = q.front();
+		q.pop();
+		if (curPos.x == endx && curPos.y == endy) {
+			return true;
+		}
+		//向四周搜索
+		for (int i = 0; i < 4; i++) {
+			int nx = curPos.x + pos[i][0];
+			int ny = curPos.y + pos[i][1];
+			if (nx < 0 || nx >= row || ny < 0 || ny >= col) {
+				continue;
+			}
+			////如果与终点坐标相同，说明找到路径了 在两个位置好像都行
+			//if (nx == endx && ny == endy) {
+			//	return true;
+			//}
+			//将能走的且未走过的子点放入队列中
+			if (grid[nx][ny] == 0 && visit[nx][ny] == 0) {
+				q.push(position(nx, ny));
+			}
+		}
+	}
+	return false;
+}
+
+int main() {
+	vector<vector<int>> grid = { { 1, 0, 0, 1 },
+	{ 0, 0, 0, 1 },
+	{ 1, 0, 0, 0 },
+	{ 1, 1, 1, 0 } };
+	bool res = BFS(grid, 3, 0, 3, 3);
+	bool res1 = BFS(grid, 1, 0, 3, 3);
+	printf("%d \n", res);
+	printf("%d \n", res1);
+	system("pause");
+	return 0;
+}
+
