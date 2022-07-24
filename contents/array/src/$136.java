4b0347e4b110d0acc2bf8ab6dd7edb44168ce520
ZhouLan <1832311982@qq.com>
2022-07-24 21:56:42 +0800

    只出现一次的数字

---
+++ contents/array/src/$136.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+/*
+只出现一次的数字
+ */
+public class $136 {
+    //只出现一次的数字
+    public static int singleNumber(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int x : nums) {
+            int count = map.getOrDefault(x,0);
+            map.put(x, count+1); //为什么count++错了
+        }
+        //遍历map，找到count为 1 的key
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+}
