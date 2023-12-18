3c732ca4ba32418b0821cddb987244ee744e2d92
Zhoulan-Jan <45385050+Zhoulan-Jan@users.noreply.github.com>
2023-12-19 00:33:23 +0800

    Delete 11088/src directory

--- 11088/src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
-        TreeNode root = new TreeNode(1);
-        System.out.println(tree.preorderTraversal(root));
-        System.out.println(tree.inorderTraversal(root)); //List<Integer>如何整体输出
-    }
-}
